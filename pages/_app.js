8f3f2e84ac770e99aff153c6e5666d296b06fcbd
Rohit kumar suman <rohitkumarsuman2011india@gmail.com>
2022-12-01 10:56:06 +0530

    yup

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
-import Head from "next/head";
+// import Head from "next/head";
 
-import "../styles/globals.css";
+// import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Metaversus</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link
-        rel="stylesheet"
-        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
-      />
-    </Head>
-    <Component {...pageProps} />
-  </>
-);
+// const MyApp = ({ Component, pageProps }) => (
+//   <>
+//     <Head>
+//       <title>Metaversus</title>
+//       <meta name="viewport" content="width=device-width, initial-scale=1" />
+//       <link rel="icon" href="/favicon.ico" />
+//       <link rel="preconnect" href="https://stijndv.com" />
+//       <link
+//         rel="stylesheet"
+//         href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+//       />
+//     </Head>
+//     <Component {...pageProps} />
+//   </>
+// );
 
-export default MyApp;
+// export default MyApp;
